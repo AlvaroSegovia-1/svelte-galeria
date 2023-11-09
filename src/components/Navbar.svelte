c56896310073c79913eb10742e2368f8de3be64c
Alvagit25 <alvarodesegovia@gmail.com>
2023-11-09 12:29:12 +0100

    primer commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,7 @@
+<script>
+  import TailwindCss from "../lib/TailwindCSS.svelte";
+</script>
+
+<div class="navbar bg-neutral text-neutral-content">
+  <a class="btn btn-ghost normal-case text-xl" href="/">daisyUI</a>
+</div>
